1d51329bdeda91301020211753fd656a806dac26
pressxtowin <i.am.anisimov@gmail.com>
2017-03-22 11:28:07 +0200

    adding .gitignore

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -7,3 +7,10 @@ BD=$(echo -e "\033[1m")
 printf "$BD$YW-> Cleaning...$NC\n"
 cd ~/Downloads
 rm jre-8u121-linux-x64.rpm
+yum remove -y gcc kernel-devel perl mlocate
+yum clean all
+# Zero out the rest of the free space using dd, then delete the written file.
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
+# Add `sync` so Packer doesn't quit too early, before the large file is deleted.
+sync
